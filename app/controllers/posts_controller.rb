cc7491d10391fb85cad445f323cac7a3a99cc45e
unknown <abdulcader.ayaz@gmail.com>
2020-08-28 23:11:06 +0200

    Part 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_post, only: [:show]
+  before_action :auth_subscriber, only: [:new]
 
   def index
     @posts = Post.all
   end
 
   def show
+    @comment = Comment.new
   end
 
   def new
@@ -29,10 +31,16 @@ class PostsController < ApplicationController
   private
 
   def find_post
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
   end
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def auth_subscriber
+    unless Subscription.where(community_id: params[:community_id], user_id: current_user.id).any?
+      redirect_to root_path, flash: { danger: "You are not authorized to view this page" }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :communities
+  has_many :subscriptions
   has_many :posts
+  has_many :comments
+  has_many :communities, through: :subscriptions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :first_name, :last_name, :username
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
